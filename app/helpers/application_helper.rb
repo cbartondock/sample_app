623138e235274b5e7861ef08ed6fcbf8b31fb404
EmADgine <matt@emadgine.com>
2014-06-01 17:12:19 -0400

    CSS

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+    #Returns the full title on a per page basis.
+    def full_title(pt)
+        base = "Ruby on Rails App"
+        if pt.empty?
+            base
+        else
+            "#{base} | #{pt}"
+        end
+    end
 end
